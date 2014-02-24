be2eeea40814613b3e72df73a1691fdbae2cdabb
Luis Sotomayor <LSotomayor@gmail.com>
2014-02-23 17:56:58 -0800

    Added tests

---
+++ AutomapperSpike/ExactMatchTest.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomapperSpike
+{
+    [TestFixture]
+    public class ExactMatchTest
+    {
+        class Source
+        {
+            public int Age { get; set; }
+            public string Name { get; set; }
+        }
+
+        class Destination
+        {
+            public int Age { get; set; }
+            public string Name { get; set; }
+        }
+        
+        [SetUp]
+        public void SetUp()
+        {
+            Mapper.Reset();
+            Mapper.CreateMap<Source, Destination>();
+        }
+
+        [Test]
+        public void ShouldBeOk()
+        {
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
---
+++ AutomapperSpike/MissingPropertyTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using AutoMapper;
+
+namespace AutomapperSpike
+{
+    public class MissingPropertyTest
+    {
+        class Source
+        {
+            public int Age { get; set; }
+        }
+
+        class Destination
+        {
+            public string Name { get; set; }
+            public int ID { get; set; }
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Mapper.Reset();
+            Mapper.CreateMap<Source, Destination>();
+        }
+
+        [Test]
+        [ExpectedException(typeof(AutoMapperConfigurationException), 
+            ExpectedMessage = "Unmapped members were found", 
+            MatchType = MessageMatch.Contains)]
+        public void ShouldThrowException()
+        {
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+
+
+}
